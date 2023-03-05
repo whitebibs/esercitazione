6ea6aa178286dc81c51bab84864fb6520af49612
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-03-05 18:56:36 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export const Navbar = () => {
   return (
     <>
-    <nav className="flex justify-center ">
+    <nav>
         <Link to="/" >Home</Link>
         <Link to="/posts">Posts</Link>
         <Link to="/todos">Todos</Link>
