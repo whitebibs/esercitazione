59cdebf25fd9b7e304865178ea3862ec7b731a75
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-03-12 23:37:55 +0100

    render list

--- src/layout/pages/Posts.js
+++ src/layout/pages/Posts.js
@@ -3,20 +3,34 @@ import  useAxios from "../../hooks/useAxios";
 const {BASE_API_URL} = Constants;
 
 export const Posts = () => {
-    const [data,error,loading,updateData] = useAxios("https://jsonplaceholder.typicode.com/posts");
+    const [data,error,loading,updateData] = useAxios(`${BASE_API_URL}/posts`);
+    console.log(data)
     if(error){
         return <h3>Error: {error.message}</h3>
     }
     if(loading){
         return <h3>Loading...</h3>
     }
+
+    const postsList = data.map((post)=>(
+      
+     <li key={post.id} className=" border-solid border-2 border-blue-300 p-3 m-2">
+        <h4 className="mb-1 text-lg text-blue-600">{post.title}</h4>
+        <p className="text-sm">{post.body}</p>
+      </li>  
+    ))
  
     return(
         <>
         <h1>Posts</h1>
         {loading
         ?<h3>Loading...</h3>
-        :<h1>I dati ci sono</h1>}
+        :<>
+        <ul>
+        {postsList}
+        </ul>
+        </>}
+            
         </>
         
     )
